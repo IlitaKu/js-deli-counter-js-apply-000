da0459130746c992c5499fbe089fb83c99ba1d4b
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-28 11:08:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ var katzDeliLine = [];
 function takeANumber(curentLine, name){
   var currentLine = [];
   curentLine.push(name);
-  return `Welcome, ${name}. You are number ${currentLine + 1} in line`; 
+  return `Welcome, ${name}. You are number ${currentLine + 1} in line.`; 
+}
+
+function nowServing(katzDeliLine){
+  for ( var i = 0; i<katzDeliLine.length; i++){
+    if (katzDeliLine ===0)
+    console.log("There is nobody waiting to be served!");
+  }
+  else 
+    katzDeliLine[0]
+  
+}
 }

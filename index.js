711aeec673ece541427af821a007a404a4ee9dc3
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-28 12:09:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine){
 function currentLine(line){
   for(var i = 0; i<line.length; i++)
   if (line === 0);
-  return " The line is currently empty.";
+  return "The line is currently empty.";
   
 }
   

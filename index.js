9b1f897f787b323fe1485737be2545c47007fdfb
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-28 11:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeliLine = [];
 function takeANumber(curentLine, name){
+  var currentLine = [];
   curentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine + 1} in line`; 
 }

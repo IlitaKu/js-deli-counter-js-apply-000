6c88622be0b5b20ab6b5c1130b7c66cd35cbc32e
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-28 11:16:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(curentLine, name){
 function nowServing(katzDeliLine){
   for ( var i = 0; i<katzDeliLine.length; i++){
     if (katzDeliLine ===0)
-    console.log("There is nobody waiting to be served!");
+    return "There is nobody waiting to be served!";
   }
      return katzDeliLine[0];
 

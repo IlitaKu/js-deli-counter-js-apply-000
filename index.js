7ab5e119a615164f82d8dc73560620b7d05abfcf
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-06-28 12:46:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var katzDeliLine = [];
 function takeANumber(curentLine, name){
-  var currentLine = [];
   curentLine.push(name);
-  return `Welcome, ${name}. You are number ${currentLine + 1} in line.`; 
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`; 
 }
 var i = 0;
 function nowServing(katzDeliLine){
@@ -13,17 +12,6 @@ function nowServing(katzDeliLine){
 }
 }
 
-
-function currentLine(line){
-  var newLine =[];
-  var i = 0;
-  if (!line.length){
-  return"The line is currently empty.";
-  } else{
-    newLine.push(` ${i+1}. ${line[i]}.`);
- return "The line is currently:"+ newLine.join(',');
-}
-}
 var katzDeliLine = [];
 
 function currentLine(x) {
